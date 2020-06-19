00a30b431e0ef638cfb71da2421e692b0c5ecec8
Yaniv Amiri <yanivam123@gmail.com>
2020-06-19 00:49:25 -0400

    Build, simple readme, made markers resize when hovered over and readjusted location of marker, created text but not finished yet

---
+++ examples/simple-example/build/precache-manifest.f5fa55fb41c7179e47d0f213a8ca4bed.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e5bebab3c33a479e16fd1ea9f43544c7",
+    "url": "/index.html"
+  },
+  {
+    "revision": "f9fc7634cf846ba92ff3",
+    "url": "/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "7801bfc80a3313b373ea",
+    "url": "/static/js/2.2407abc4.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.2407abc4.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f9fc7634cf846ba92ff3",
+    "url": "/static/js/main.4ad21d73.chunk.js"
+  },
+  {
+    "revision": "216563cf50e7c35b2f13",
+    "url": "/static/js/runtime-main.7c0ae09a.js"
+  }
+]);
--- src/ReactPathMarker.tsx
+++ src/ReactPathMarker.tsx
@@ -11,44 +11,56 @@ interface IProps {
 }
 
 export const PathMarker: React.FC<IProps> = (props) => {
-    //const [fontSize, ] = useState (props["fontSize"] || 12)
-    //const [fontFamily, ] = useState (props["fontFamily"] || "sans-serif")
+    const [fontSize, ] = useState (props["fontSize"] || 10)
+    const [fontFamily, ] = useState (props["fontFamily"] || "sans-serif")
+    const [scale, setScale] = useState("0.07,0.07")
     const [bgColor, ] = useState (props["bgColor"] || "black")
-    //const [textColor, ] = useState (props["textColor"] || "white")
+    const markerRef = React.createRef<SVGPathElement>()
+    const semiCircle = "M256,0C153.755,0,70.573,83.182,70.573,185.426c0,126.888,165.939,313.167,173.004,321.035    c6.636,7.391,18.222,7.378,24.846,0c7.065-7.868,173.004-194.147,173.004-321.035C441.425,83.182,358.244,0,256,0z M256,278.719    c-51.442,0-93.292-41.851-93.292-93.293S204.559,92.134,256,92.134s93.291,41.851,93.291,93.293S307.441,278.719,256,278.719z"
+    const [textColor, ] = useState (props["textColor"] || "black")
     const pathRef = props.pathRef
     const svgRef = props.svgRef
-    const [markerRect, setMarkerRect] = useState({ x: 0, y: 0, w: 0, h: 0})
-    //const textRef = React.createRef<SVGTextElement>()
+    const [markerRect, setMarkerRect] = useState({ x: 0, y: 0, w: 0, h: 0, isLeft: false})
+    const textRef = React.createRef<SVGTextElement>()
 
     useEffect(() => {
         const updateMarker = () => {
-            if(svgRef && pathRef && svgRef.current && pathRef.current) {
+            if(svgRef && pathRef && svgRef.current && pathRef.current && markerRef && markerRef.current && textRef && textRef.current) {
                 const svgRect = svgRef.current.getBoundingClientRect()
                 const pathRect = pathRef.current.getBoundingClientRect()
+                const markerRect = markerRef.current.getBoundingClientRect()
+                const textRect = textRef.current.getBoundingClientRect()
 
-                const w = pathRect.width
-                const h = pathRect.height
-                const x = (pathRect.x + 0.5 * w - svgRect.x) - 20
-                const y = (pathRect.y + 0.5 * h - svgRect.y)  - 38
+                const isLeft = ((textRect.x - svgRect.x) > (svgRect.width / 2))
 
-                setMarkerRect({ x: x, y: y, w: w, h: h})
+                const w = markerRect.width
+                const h = markerRect.height
+                const x = (pathRect.x + 0.5 * pathRect.width - svgRect.x) - (w * 0.7)
+                const y = (pathRect.y + 0.5 * pathRect.height - svgRect.y) - h
+
+                setMarkerRect({ x: x, y: y, w: w, h: h, isLeft: isLeft})
             }
         }
 
         if (pathRef && pathRef.current) {
            window.addEventListener('resize', () => { updateMarker() })
            window.addEventListener("load", () => { updateMarker() })
-          }
-        }, [pathRef, svgRef])
+        }
+    }, [pathRef, svgRef, markerRef, textRef])
 
-    const scale = "0.08,0.08"
-    const semiCircle = "M256,0C153.755,0,70.573,83.182,70.573,185.426c0,126.888,165.939,313.167,173.004,321.035    c6.636,7.391,18.222,7.378,24.846,0c7.065-7.868,173.004-194.147,173.004-321.035C441.425,83.182,358.244,0,256,0z M256,278.719    c-51.442,0-93.292-41.851-93.292-93.293S204.559,92.134,256,92.134s93.291,41.851,93.291,93.293S307.441,278.719,256,278.719z"
-    const transform = "translate(" + (markerRect.x) + "," + (markerRect.y) + ")\nscale(" + scale + ")"
-    console.log(markerRect)
-    console.log(transform)
     return (
-        <g pointerEvents={"none"} >
-           <path transform={transform} fill={bgColor} d={semiCircle}></path>
+        <g>
+           <path 
+                ref={markerRef}
+                onMouseOver={() => {setScale("0.08, 0.08")}}
+                onMouseOut={() => {setScale("0.07,0.07")}}
+                transform={"translate(" + (markerRect.x) + "," + (markerRect.y) + ")\nscale(" + scale + ")"}
+                fill={bgColor}
+                d={semiCircle}
+            />
+            <text ref={textRef} x={(markerRect.isLeft) ? markerRect.x + markerRect.w + 10 : markerRect.x - markerRect.w - 10} cursor={"default"} y={markerRect.y + markerRect.h} fontFamily={fontFamily} fontSize={fontSize} fill={textColor} visibility={"hidden"}>
+                {props.smallText}
+            </text>
         </g>
     )
 }
